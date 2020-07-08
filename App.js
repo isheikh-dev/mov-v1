92ddec0204da57011f3f55c49f5f355f231ca523
isheikh-dev <ishiekh.dev.v1@gmail.com>
2020-07-08 14:47:40 +0200

    made todo list

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, View, Button, TextInput , Text } from "react-native";
 
 export default function App() {
+  const [enteredGoal , setEnterdGoal] = useState('');
+  const [courseGoals , setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => { 
+    setEnterdGoal(enteredText);
+  };
+
+  const addGoalHandler = () => { 
+    setCourseGoals(courseGoals => [...courseGoals , enteredGoal] )
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.inputStyle}
+          value={enteredGoal}
+          onChangeText={goalInputHandler}
+        />
+        <Button title="ADD"  onPress={addGoalHandler} />
+      </View>
+      <View>
+          {courseGoals.map( (goal) =>  
+              <View style={styles.listItem} key={goal + Math.random()}>
+                <Text >{goal}</Text>
+              </View>
+           )}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50,
   },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  inputStyle: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 4,
+  }, 
+  listItem: { 
+    padding: 10,
+    backgroundColor: 'grey', 
+    borderWidth:1,
+    borderColor:'black', 
+    marginVertical: 10
+  }
 });
